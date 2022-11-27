dc0e290ebe0a940e18b9f395c75f6185b89c75cb
alchemyyt <josemontesdeoca00@gmail.com>
2022-11-27 15:57:51 -0400

    creacion de la carpeta scripts

---
+++ scripts/create-env.js
@@ -0,0 +1,7 @@
+const fs = require("fs");
+//Requiere de file system que nos permite trabajar con el SO
+//Nos permite hacer, guardar o enviar datos en archivos de nuestra computadora
+
+fs.writeFileSync("./.env", `API=${process.env.API}\n`);//la variable api la sacamos de netlify tenemos que crearla en netlify primero    
+//Creamos el archivo y lo llenamos con la información que necesitamos
+//Este script se corre en el server.
